62248c163a790f4d92bb7d14ceb14c9d41d0a7d7
MarinoManalo <inomanalo23@gmail.com>
2024-11-21 01:18:07 +0800

    database succesfully connected

--- functions.php
+++ functions.php
@@ -1,3 +1,18 @@
 <?php    
-    // All project functions should be placed here
+ function connectDatabase(): mysqli {
+    $servername = 'localhost';
+    $username = 'root';        
+    $password = "";            
+    $dbname = 'dct-ccs-finals'; 
+
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
 ?>
